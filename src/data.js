fd4bf743bb513cf0eb61bbd873c983570ec57b11
FreeMasen <RFM@Herman.local>
2017-03-13 23:43:30 -0500

    establish data storage scheme (need final path)

--- src/data.js
+++ src/data.js
@@ -1,21 +1,56 @@
 const fs = require('fs');
+const { EventEmitter } = require('events');
 
 function File() {
-    this.name = 
-    this.path = process.env.HomePath + '/Documents/Time Manager/' + this.name
-    this.list = this.readIn()
+    this.name = getFileName();
+    this.path = process.env.HomePath + '/Documents/Time Manager/' + this.name;
+    this.list = this.readIn();
+    this.eventEmitter = new EventEmitter();
+    this.eventEmitter.on('update', _ => {
+        var json;
+        try {
+            json = JSON.stringify(this.list);
+        } catch (_) {
+            throw new Error('Unable to stringify JSON');
+        }
+        if (json) {
+            fs.writeFileSync(json);
+        }
+    })
 }
 
 File.prototype.readIn = function() {
     var text = fs.readFileSync(this.path);
-    this.list = JSON.parse(text.trim())
+    this.list = JSON.parse(text.trim());
 }
 
 File.prototype.save = function() {
     fs.writeFileSync(this.path, JSON.stringify(this.list));
 }
 
-function getFileName {
+File.prototype.add = function(item) {
+    this.list.add(this);
+    this.eventEmitter.emit('update')
+}
+
+File.prototype.find = function(id) {
+    this.list.forEach(item => {
+        if (item.id == id) return item
+    })
+}
+
+File.prototype.update = function(id, update) {
+    var element = this.find(id);
+    if (!element) return false;
+    for (var k in update) {
+        element[k] = update[k];
+    }
+    this.eventEmitter.emit('update')
+    return true;
+}
+
+
+function getFileName() {
     var dt = new Date()
     return `${dt.getFullYear}.${twoDigits(dt.getMonth)}.${twoDigits(dt.getDate)}.Time.Manager.json`
 }
@@ -26,3 +61,6 @@ function twoDigits(number) {
     }
     return `${number}`
 }
+
+
+module.exports = File;
