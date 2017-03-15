db954c505de1ce8f2417295973196690841f7ce9
FreeMasen <RFM@Herman.local>
2017-03-14 22:47:49 -0500

    remove boilerplate comments

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ const Positioner = require('electron-positioner')
 
 const db = require('./src/data');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let win;
 let icon;
 let positioner;
@@ -29,9 +27,6 @@ function createWindow () {
 
   // Emitted when the window is closed.
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     win = null
   });
 
@@ -66,23 +61,9 @@ function createWindow () {
     }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
