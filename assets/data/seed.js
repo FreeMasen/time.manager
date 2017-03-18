e56502a41b000fa6a8afd6736e5002efde732e78
FreeMasen <RFM@Herman.local>
2017-03-18 00:00:55 -0500

    working menu seed

--- assets/data/seed.js
+++ assets/data/seed.js
@@ -1,42 +1,40 @@
-const Ned = require('nedb');
+const Database = require('../../src/dataBase.js');
+const db = new Database('time.manager', ['menu', 'work']);
+const { MenuItem } = require('electron');
 
-const work = new Ned({
-    filename: `assets/data/time.manager.work.db`,
-    timestampData: true
-});
-console.log(work.filename)
-
-const menu = new Ned({
-    filename: `assets/data/time.manager.menu.db`
-})
-
-work.loadDatabase(err => {
-    console.error('Work Error: ', err);
-})
-
-menu.loadDatabase(err => {
-    if (err) return console.error('Menu Error: ', err)
-    menu.insert([{
+var menuTemplate = [
+    {
         label: 'File',
-        accelerator: 'CmdOrCtrl+R',
-        click (item, focusedWindow) {
-        focusedWindow.loadUrl(__dirname + '/index.html');
-        }
-    },{
-        label: 'Close Window',
-        accelerator: 'CmdOrCtrl+W',
-        click(item, focusedWindow) {
-            focusedWindow.close();
-        }
-        },{
-        label: 'Quit',
-        accelerator: 'CmdOrCtrl+Q',
-        click(item, focusedWindow) {
-            app.quit();
-        }
-        }], (err, doc) => {
-            if (err) return console.log('Menu Seed Error: ', err);
-            if (doc) console.log(doc)
-        })
+        submenu: [
+            {
+                label: 'Reload',
+                accelerator: 'CmdOrCtrl+R',
+                click (item, focusedWindow) {
+                    focusedWindow.loadUrl(__dirname + '/index.html');
+                }
+            },
+            {
+                role: 'close'
+            },
+            {
+                role: 'quit'
+            }
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+        ]
+    }
+]
+
+db.menu.insert(menuTemplate, (err, doc) => {
+    if (err) return console.log('Menu Seed Error: ', err);
+    if (doc) console.log(doc)
 })
 
