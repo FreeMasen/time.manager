5fe2f3d67d3ee5d5289be740d01a40f3265e0cce
FreeMasen <RFM@Herman.local>
2017-03-18 23:46:44 -0500

    add electron declare statement

--- app/dashboard/component.ts
+++ app/dashboard/component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-
+declare var electron: any;
 @Component({
     selector:'<dashboard>',
     templateUrl: 'app/dashboard/template.html',
     styleUrls: ['app/dashboard/style.css']
 })
 export class Dashboard implements OnInit {
-
+    
     ngOnInit():void {
-
     }
 }
