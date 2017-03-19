cac7523ff9444a48089742c7e0834e9d7f07c97a
FreeMasen <RFM@Herman.local>
2017-03-18 23:48:19 -0500

    updated models

---
+++ app/models/task.ts
@@ -0,0 +1,7 @@
+import { Work } from './work';
+export class Task {
+    _id: string;
+    objective: string;
+    notes: string[];
+    work: Work[];
+}
--- app/models/work.ts
+++ app/models/work.ts
@@ -1,19 +1,5 @@
-
 export class Work {
-    id: number;
+    _id: string;
     start: Date;
     duration: number;
-    categoryId: string;
-
-    constructor(
-        id: number = -1,
-        start: Date = new Date(0),
-        duration: number = -1,
-        categoryId: string = ''
-    ) {
-        this.id = id;
-        this.start = start;
-        this.duration = duration;
-        this.categoryId = categoryId;
-    }
 }
