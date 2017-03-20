f5051c37bb0e8ab168b70322e3d155be427d59e7
FreeMasen <RFM@Herman.local>
2017-03-19 23:46:11 -0500

    add isComplete and constructor

--- app/models/task.ts
+++ app/models/task.ts
@@ -4,4 +4,17 @@ export class Task {
     objective: string;
     notes: string[];
     work: Work[];
+    isComplete: boolean;
+    
+    constructor(id: string = '-1',
+                objective: string = '',
+                notes: string[] = [],
+                work: Work[] = [],
+                isComplete: boolean = false) {
+        this._id = id;
+        this.objective = objective;
+        this.notes = notes;
+        this.work = work;
+        this.isComplete = isComplete;
+    }
 }
