4678d7bcf303a1912301f20c9d052e71e1ff7237
Robert Masen <rmasen@pdastats.com>
2017-03-24 12:18:56 -0500

    Working task-list-item animations

--- app/taskListItem/component.ts
+++ app/taskListItem/component.ts
@@ -11,12 +11,24 @@ import { Task } from '../models';
     animations: [
         trigger('drawer', [
             transition('void => *', [
-                animate(250, style({
+                animate('250ms ease-in', style({
                     height: '75px'}))
             ]),
             transition('* => void', [
-                animate(250, style({
+                animate('250ms ease-out', style({
                     height: 0}))
+            ])
+        ]),
+        trigger('expanded', [
+            state('collapsed', style({transform: 'rotate(0)'})),
+            transition('collapsed => expanded', [
+                animate(250, style({
+                    transform: 'rotate(90deg)'}))
+            ]),
+            state('expanded', style({transform: 'rotate(90deg)'})),
+            transition('expanded => collapsed', [
+                animate(250, style({
+                    transform: 'rotate(0)'}))
             ])
         ])
     ]
@@ -24,7 +36,7 @@ import { Task } from '../models';
 export class TaskListItem {
 
     expanded: string = 'collapsed';
-    drawer: string = 'closed';
+    
     @Input() task: Task;
 
     constructor() {
@@ -32,11 +44,20 @@ export class TaskListItem {
     }
 
     toggleExpanded(): void {
-        if (this.expanded = 'collapsed') {
-            this.expanded = 'expanded'
+        console.log(`updating from: ${this.expanded}, ${this.drawer}`);
+        if (this.expanded == 'collapsed') {
+            this.expanded = 'expanded';
         } else {
-            this.expanded = 'collapsed'
+            this.expanded = 'collapsed';
         }
+        console.log(`to: ${this.expanded}, ${this.drawer}`);
+    }
+
+    get drawer(): string {
+        if (this.expanded == 'collapsed') {
+            return 'closed'
+        }
+        return 'open'
     }
 
     toggleDrawer(): string {
