e57627758c5298acaeb88ab89d9291b77adb502f
Robert Masen <rmasen@pdastats.com>
2017-04-18 13:38:10 -0500

    Add seed warning messages

--- app/data/service.ts
+++ app/data/service.ts
@@ -5,14 +5,22 @@ import { Seed } from './seed';
 
 @Injectable()
 export class Data {
-    tasks = new Collection<Task>('./app/data/tasks.db');
+    tasks = new Collection<Task>('./app/data/tasks.db', this.listenToCollection);
     categories = new Collection<Category>('./app/data/categories.db');
     clients = new Collection('./app/data/clients.db');
 
     seed() {
+        console.error('SEEDING DATA STORE FILES')
         var seed = new Seed();
+        console.error('SEEDING TASKS')
         this.tasks.insertBulk(seed.tasks());
+        console.error('SEEDING CATEGORIES');
         this.categories.insertBulk(seed.categories());
+        console.error('SEEDING CLIENTS');
         this.clients.insertBulk(seed.clients());
+    }
+
+    listenToCollection(msg: any) {
+        console.log(msg);
     }
 }
