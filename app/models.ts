e1cae2bdab4f28c4088e98aa96e92431eb627da5
FreeMasen <RFM@Herman.local>
2017-04-19 16:41:49 -0500

    remove interface

--- app/models.ts
+++ app/models.ts
@@ -2,7 +2,4 @@ export {Client} from './modelDefs/client'
 export {Task} from './modelDefs/task'
 export {Work} from './modelDefs/work'
 export {Category} from './modelDefs/category'
-export interface Storeable {
-    _id: string;
-}
 export { Collection } from './modelDefs/collection'
