5e08ddbd7d0966e3ae8a29b77c05ad1f25e27f31
Robert Masen <RMasen@pdastats.com>
2017-04-24 15:07:10 -0500

    add date calculatrions

--- app/calculator/service.ts
+++ app/calculator/service.ts
@@ -1,13 +1,39 @@
+
 import { Injectable } from '@angular/core';
-import { Work } from '../models';
+
+import { Data } from '../services';
+import { Work, Bucket, Task } from '../models';
+
+
 @Injectable()
 export class Calculator {
+
+    constructor(private data: Data) {}
+
     totalMinutesOfWork(work: Work[]): number {
         var ret =  work.map(work => {
             return work.duration;
         }).reduce((acc, val) => {
             return acc + val;
         }, 0);
+        return ret;
+    }
+
+    getMonday(dt: Date): Date {
+        return this.getDay(dt, 1,true);
+    }
+
+    getSunday(dt: Date): Date {
+        return this.getDay(dt, 0)
+    }
+
+    getDay(dt: Date, targetDay: number, backwards: boolean = false): Date {
+        var ret = new Date(dt.getFullYear(), dt.getMonth(), dt.getDate())
+        
+        while (ret.getDay() != targetDay) {
+            if (backwards) ret.setDate(ret.getDate() - 1)
+            else ret.setDate(ret.getDate() + 1)
+        }
         return ret;
     }
 }
