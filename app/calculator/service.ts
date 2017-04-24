9e2447de9f2f0a930e2aa7cb74a3a6d4d89dd918
FreeMasen <RFM@Herman.local>
2017-04-23 23:23:33 -0500

    remove logging

--- app/calculator/service.ts
+++ app/calculator/service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Task } from '../models';
+import { Work } from '../models';
 @Injectable()
 export class Calculator {
-    totalMinutesOfWork(task: Task): number {
-        var ret =  task.work.map(work => {
+    totalMinutesOfWork(work: Work[]): number {
+        var ret =  work.map(work => {
             return work.duration;
         }).reduce((acc, val) => {
             return acc + val;
         }, 0);
-        console.log(`totalMinutesOfWork(): ${ret}`);
         return ret;
     }
 }
