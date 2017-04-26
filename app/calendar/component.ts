bbba5b2affc80c8fd2307909e1109b6aa7de1e78
FreeMasen <RFM@Herman.local>
2017-04-26 07:24:31 -0500

    add method for capturing total worked per day

--- app/calendar/component.ts
+++ app/calendar/component.ts
@@ -5,30 +5,54 @@ import { Task } from '../models';
 
 @Component({
     selector: '<calendar>',
+    outputs: ['task', 'startDate', 'endDate'],
     templateUrl: './template.html',
     styleUrls: ['./style.css']
 })
 export class Calendar implements OnInit {
     referenceDate = new Date();
     tasks: Task[] = [];
-
+    work: number[] = [
+        0,//u
+        0,//m
+        0,//t
+        0,//w
+        0,//r
+        0,//f
+        0,//s
+    ];
     constructor(private data: Data,
                 private dateFormatter: DateFormatter,
                 private calculator: Calculator) {}
 
     ngOnInit() {
+        this.getTasks();
+    }
+
+    getTasks(): void {
         this.data.tasks.find({})
             .then(tasks => {
                 this.tasks = tasks;
+                this.getWork();
             })
+    }
+
+    getWork(): void {
+        var ids = this.tasks.map(task => {
+                return task._id;
+            })
+            this.data.work.find({taskId: {$in: ids}})
+                .then(work => {
+                    this.work = this.calculator.weekOfWorkMinutes(work);
+                })
     }
 
     formatHours(hours: number) {
         return this.dateFormatter.hoursWithDecimal(hours);
     }
 
-    getTotal(day: string): string {
-        return this.dateFormatter.hoursWithDecimal(8);
+    getTotal(day: number): string {
+        return this.dateFormatter.hoursWithDecimal(this.work[day]);
     }
 
     goBack() {
